cee6f0b0848d199182d87df2f3f7a43200b3b640
pablo kitzberger <kitzberger.pablo@gmail.com>
2025-10-30 19:00:43 -0300

    Updated index.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
 "use client";
 
-import { BettingCard } from "@/components";
 import styles from "./landing.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.root}>
-      <BettingCard
-        imageUrl="https://images.pexels.com/photos/1618200/pexels-photo-1618200.jpeg?auto=compress&cs=tinysrgb&w=800"
-        status="In-Play"
-        question="Will Dak Prescott record 275+ passing yards vs Cardinals?"
-        yesPercentage={44}
-        noPercentage={56}
-        participants={35}
-        totalPool="$3.71k"
-        endDate="Nov 4"
-      />
-    </div>
-  );
+  return <div className={styles.root}>comming soon...</div>;
 }
--- src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { BettingCard } from './BettingCard';
